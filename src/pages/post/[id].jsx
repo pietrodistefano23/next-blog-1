516ee4d4bdcf1a8f7272d8427de332c490622653
pietrodistefano23 <pietrodistefano2306@gmail.com>
2023-02-03 17:50:08 +0100

    add new project next-blog

--- src/pages/post/[id].jsx
+++ src/pages/post/[id].jsx
@@ -1,23 +1,36 @@
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
 import { GET } from "@/utils/api";
 import styles from "@/styles/dPost.module.scss";
-
+import {RxExit} from "react-icons/rx";
 export default function () {
   const router = useRouter();
   let { id } = router.query;
+ 
+  const imgPost = "https://picsum.photos/600/400?"
 
   const [postData, setPostData] = useState({});
 
   useEffect(() => {
+    if(router.isReady)
     GET(`posts/${id}`).then((data) => setPostData(data));
-  }, []);
+  }, [router.isReady]);
 
   return (
     <div className={styles.dPost}>
-      <img src={postData.image} alt={postData.title} />
+      <Image 
+      src={imgPost+id}
+      width={450}
+      height={250}
+      alt='image'
+      />
       <h1>{postData.title}</h1>
       <p>{postData.body}</p>
+      <div className={styles.container}>
+          <Link href='/'><RxExit/></Link>
+      </div>
     </div>
   );
 }
